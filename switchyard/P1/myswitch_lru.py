4d32150621c72f38a4c707c09c789d53eb1e01ce
Melanie Rogers <mrogers8@melanies-mbp.home>
2019-03-03 17:52:19 -0600

    first 11 tests LRU complete

--- switchyard/P1/myswitch_lru.py
+++ switchyard/P1/myswitch_lru.py
@@ -13,21 +13,29 @@ class TableEntry():
         self.mac = mac
         self.timestamp = timestamp
 
-def get_table_entry(fw_table, interface):
+def get_table_entry(fw_table, address):
     # TODO return the table entry if it exists
     #  should return the index of the entry or false if there isn't an entry
-    #  return {entry, index}
-    print("GET_TABLE_ENTRY : {} : {}".format(fw_table, interface))
+    #  Return: {entry, index}
+    print("GET_TABLE_ENTRY") # : {} : {}".format(fw_table, interface))
     for i in range(len(fw_table)):
-        print("   fw_table[{}]={}".format(i,fw_table[i]))
-        if fw_table[i].intf == interface:
+        #print("   fw_table[{}]={}".format(i,fw_table[i]))
+        if fw_table[i].mac == address:
             return fw_table[i],i
 
-    return False, 0
-
+    return False, -1
 
 def insert_table_entry(fw_table, entry: TableEntry, index_to_remove):
     # TODO: this maintains the LRU format of the table
+    #  index_to_remove = -1 if not in table, >0 if valid entry
+    #  Return: none
+    print("INSERT_TABLE_ENTRY : index={} : len={}".format(index_to_remove, len(fw_table)))
+    if index_to_remove >= 0:
+        #Replace Index
+        fw_table.insert(index_to_remove, entry)
+    else:
+        #Add Entry (replace LRU if full, else add)
+        fw_table.append(entry)
     return
 
 
@@ -44,21 +52,23 @@ def main(net):
         except Shutdown:
             return
 
+        print("~~")
         print("Packet sent from {} on input_port={} destined for: {}".format(packet[0].src, input_port, packet[0].dst))
         if packet[0].dst in mymacs:
             print("Packet intended for me. Just drop it")
         else:
-            entry, index = get_table_entry(fw_tbl, input_port)
             new_entry = TableEntry(input_port, packet[0].src, timestamp)
-            if entry and entry.mac == packet[0].dst:
-                print("We know destination so forward")
-                net.send_packet(input_port, packet)
-                insert_table_entry(fw_tbl, new_entry, index)
-                continue
-            else:
-                insert_table_entry(fw_tbl, new_entry, index)
 
-                #this is simply doing the broadcast since we dont know the MAC
+            src_entry, src_index = get_table_entry(fw_tbl, packet[0].src)
+            dst_entry, dst_index = get_table_entry(fw_tbl, packet[0].dst)
+
+            insert_table_entry(fw_tbl, new_entry, src_index)
+
+            if dst_entry :
+                print("We know destination so forward")
+                net.send_packet(dst_entry.intf, packet)
+            else:
+                #this is simply doing the broadcast since we don't know the MAC
                 for intf in my_interfaces:
                     if input_port != intf.name:
                         print("Flooding packet {} to {}".format(packet, intf.name))
