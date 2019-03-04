50c9750ff58a68eb7f35f902e8bdc5870512dbab
Melanie Rogers <mrogers8@melanies-mbp.home>
2019-03-03 18:32:54 -0600

    Given LRU Tests Passing

--- switchyard/P1/myswitch_lru.py
+++ switchyard/P1/myswitch_lru.py
@@ -19,30 +19,45 @@ def get_table_entry(fw_table, address):
     #  Return: {entry, index}
     print("GET_TABLE_ENTRY") # : {} : {}".format(fw_table, interface))
     for i in range(len(fw_table)):
-        #print("   fw_table[{}]={}".format(i,fw_table[i]))
         if fw_table[i].mac == address:
             return fw_table[i],i
 
     return False, -1
 
-def insert_table_entry(fw_table, entry: TableEntry, index_to_remove):
+def insert_table_entry(fw_table, entry: TableEntry, index, ifDst):
     # TODO: this maintains the LRU format of the table
-    #  index_to_remove = -1 if not in table, >0 if valid entry
+    #  Parameters:
+    #       index: -1 if not in table, else 0-4
+    #       ifDst: automatically Most Recently Used if dst, False if src
+    #  LRU: [Least Recent, ... ,Most Recent]
     #  Return: none
-    print("INSERT_TABLE_ENTRY : index={} : len={}".format(index_to_remove, len(fw_table)))
-    if index_to_remove >= 0:
-        #Replace Index
-        fw_table.insert(index_to_remove, entry)
+    print("INSERT_TABLE_ENTRY : index={} : len={}".format(index, len(fw_table)))
+
+    if index >= 0:
+        # Update table
+        if ifDst:
+            fw_table.pop(0)
+            fw_table.append(entry)
+        else:
+            fw_table.pop(index)
+            fw_table[index].intf = entry.intf
+    elif index == -1:
+        # Add New Entry
+        if len(fw_table) <= 5:
+            fw_table.append(entry)
+        else:
+            # Remove LRU & Add
+            fw_table.pop(0)
+            fw_table.append(entry)
     else:
-        #Add Entry (replace LRU if full, else add)
-        fw_table.append(entry)
+        print("ERROR")
     return
 
 
 def main(net):
     my_interfaces = net.interfaces()
     mymacs = [intf.ethaddr for intf in my_interfaces]
-    fw_tbl = [] #this is where we will maintain our forward table
+    fw_tbl = []  # this is where we will maintain our forward table
 
     while True:
         try:
@@ -57,16 +72,19 @@ def main(net):
         if packet[0].dst in mymacs:
             print("Packet intended for me. Just drop it")
         else:
-            new_entry = TableEntry(input_port, packet[0].src, timestamp)
+            new_src_entry = TableEntry(input_port, packet[0].src, timestamp)
 
             src_entry, src_index = get_table_entry(fw_tbl, packet[0].src)
             dst_entry, dst_index = get_table_entry(fw_tbl, packet[0].dst)
 
-            insert_table_entry(fw_tbl, new_entry, src_index)
+            insert_table_entry(fw_tbl, new_src_entry, src_index, False)
 
-            if dst_entry :
+            if dst_entry:
                 print("We know destination so forward")
                 net.send_packet(dst_entry.intf, packet)
+                #update dst entry in table
+                dst_entry.timestamp = timestamp
+                insert_table_entry(fw_tbl, dst_entry, dst_index, True)
             else:
                 #this is simply doing the broadcast since we don't know the MAC
                 for intf in my_interfaces:
