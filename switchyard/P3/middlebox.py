45ae27316c4d17597b1a0ad56449cc7103b6700c
cjswedes <cswedal@wisc.edu>
2019-04-25 20:56:49 -0500

    I think the middle box has everything in it. Need to double check src and dst ethernet header. I dont knoww how to test this.

--- switchyard/P3/middlebox.py
+++ switchyard/P3/middlebox.py
@@ -16,9 +16,28 @@ def switchy_main(net):
     mymacs = [intf.ethaddr for intf in my_intf]
     myips = [intf.ipaddr for intf in my_intf]
 
-    # random.seed(random_seed) #Extract random seed from params file
-    # 
+    # extract interface objects
+    blaster_intf = None
+    blastee_intf = None
+    for intf in my_intf:
+        if intf.name == 'eth0':
+            blaster_intf = intf
+        elif intf.name == 'eth1':
+            blastee_intf = intf
+    if not blaster_intf or not blastee_intf:
+        print('error getting middlebox interfaces')
+        assert False
 
+    # Parsing the arguments file?
+    with open('middlebox_params.txt') as params:
+        args = params.readline().split()
+    if len(args) < 4:
+        print('Arguments issue')
+        assert False
+    percent = int(args[3])
+    random_seed = int(args[1])
+
+    random.seed(random_seed) #Extract random seed from params file
 
     while True:
         gotpkt = True
@@ -41,17 +60,29 @@ def switchy_main(net):
             Received data packet
             Should I drop it?
 
-
             If not, modify headers & send to blastee
             '''
+            if drop(percent=percent):
+                continue
+
+            # Create new Ethernet Header for the packet
+            # TODO: dooublle check the src and dest
+            eth_header = Ethernet(src=blaster_intf.ethaddr,
+                                  dst=blastee_intf.ethaddr,
+                                  ethertype=EtherType.IPv4)
+            pkt[0] = eth_header
             net.send_packet("middlebox-eth1", pkt)
         elif dev == "middlebox-eth1":
             log_debug("Received from blastee")
             '''
             Received ACK
             Modify headers & send to blaster. Not dropping ACK packets!
-            net.send_packet("middlebox-eth0", pkt)
             '''
+            eth_header = Ethernet(src=blastee_intf.ethaddr,
+                                  dst=blaster_intf.ethaddr,
+                                  ethertype=EtherType.IPv4)
+            pkt[0] = eth_header
+            net.send_packet("middlebox-eth0", pkt)
         else:
             log_debug("Oops :))")
 
